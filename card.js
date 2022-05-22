009fe0d30d56715bc7cae2ddb71ec8d651ce0dd6
Subham Shah <sshah0626@gmail.com>
2022-05-22 21:56:08 +0545

    Card Details with view more button

---
+++ card.js
@@ -0,0 +1,36 @@
+function card(){
+    let s=document.getElementById("intro");
+    if(s.style.display=="none"){
+        s.style.display="block";
+    }
+        else{
+            s.style.display="none";
+        }
+}
+function aboutus(){
+    let s=document.getElementById("abt");
+    if(s.style.display=="none"){
+        s.style.display="block";
+    }
+        else{
+            s.style.display="none";
+        }
+}
+function services(){
+    let s=document.getElementById("ser");
+    if(s.style.display=="none"){
+        s.style.display="block";
+    }
+        else{
+            s.style.display="none";
+        }
+}
+function contact(){
+    let s=document.getElementById("con");
+    if(s.style.display=="none"){
+        s.style.display="block";
+    }
+        else{
+            s.style.display="none";
+        }
+}
